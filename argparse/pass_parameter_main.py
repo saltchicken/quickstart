e7d2102c5fca26f03ddc7aec44bfb295f710669d
John <3859906+saltchicken@users.noreply.github.com>
2019-12-07 20:08:38 -0800

    Update pass_parameter_main.py

--- argparse/pass_parameter_main.py
+++ argparse/pass_parameter_main.py
@@ -1,6 +1,6 @@
 import argparse
 
-def example_pass_arguments(arg1: int, arg2: int):
+def main(arg1: int, arg2: int):
     print(arg1, arg2)
 
 if __name__ == "__main__":
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--arg1", type=int, default=0)
     parser.add_argument("-b", "--arg2", type=int, default=0)
     ns = parser.parse_args()
-    example_pass_arguments(**ns.__dict__)
+    main(**ns.__dict__)
