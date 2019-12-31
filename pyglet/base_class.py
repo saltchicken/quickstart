27af5d718c4388d8c9f3d7ffb942dd40a4016937
John <3859906+saltchicken@users.noreply.github.com>
2019-12-31 15:41:06 -0800

    Update base_class.py

--- pyglet/base_class.py
+++ pyglet/base_class.py
@@ -6,21 +6,21 @@ from pyglet.gl import *
 class Window(pyglet.window.Window):
 	def __init__(self, *args, **kwargs):
 		super(Window, self).__init__()
+		self.batch = pyglet.graphics.Batch()
+		self.vertex_list = self.batch.add(4, pyglet.gl.GL_QUADS, None,
+						  ('v2i', [200, 200, 300, 200, 300, 100, 200, 100]),
+						  ('c4B', [200, 200, 220, 255] * 4)
 
 	def on_draw(self):
-		glClear(GL_COLOR_BUFFER_BIT)
-		glLoadIdentity()
-		glBegin(GL_TRIANGLES)
-		glVertex2f(0, 0)
-		glVertex2f(10, 0)
-		glVertex2f(10, 10)
-		glEnd()
+		pyglet.gl.glClearColor(1,1,1,1)
+		self.clear()
+		self.batch.draw()
 	
 	def on_key_press(self, symbol, modifiers):
 		if symbol == key.ESCAPE:
-			print('cool')
 			pass
-			#return pyglet.event.EVENT_HANDLED
+			# This line will prevent the ESCAPE key from closing window						  
+			# return pyglet.event.EVENT_HANDLED
 
 
 if __name__ == "__main__":
