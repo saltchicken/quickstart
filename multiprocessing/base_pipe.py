56032e35d97564cef75858a691047c6ff8841b78
John <3859906+saltchicken@users.noreply.github.com>
2019-12-31 19:18:01 -0800

    Update base_pipe.py

--- multiprocessing/base_pipe.py
+++ multiprocessing/base_pipe.py
@@ -1,28 +1,28 @@
 from multiprocessing import Process, Pipe
-import time
-import numpy as np
 
 def reader_proc(pipe):
     p_output, p_input = pipe
-    p_input.close()
+    p_input.close()     #Reading only, input unnecessary
     while True:
         msg = p_output.recv()
+        print(msg)
         if msg=='DONE':
             break
 
-def writer(count, p_input):
-    test = np.array([0]*52400000)
-    p_input.send(str(test))
+def writer(pipe):
+    p_output, p_input = pipe
+    p_output.close()    #Writing only, output unnecessary
+    msg = "test message"
+    p_input.send(msg)
     p_input.send('DONE')
 
 if __name__=='__main__':
-    for count in [1024]:
-        p_output, p_input = Pipe()
-        reader_p = Process(target=reader_proc, args=((p_output, p_input),))
-        reader_p.daemon = True
-        reader_p.start()
-
-        p_output.close()
-        writer(count, p_input)
-        p_input.close()
-        reader_p.join()
+    p_output, p_input = Pipe()
+    reader_p = Process(target=reader_proc, args=((p_output, p_input),))
+    reader_p.daemon = True
+    reader_p.start()
+    writer((p_output, p_input))
+    
+    p_input.close()
+    p_output.close()
+    reader_p.join()
